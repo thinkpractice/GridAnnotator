7c787541f43f556a11eb71fe7c04f7f31665c8ed
Tim De Jong <tjadejong@thinkpractice.nl>
2019-02-06 13:35:46 +0100

    Changed configuration file.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config(object):
     CLASSIFICATION_DIR = "classifications/"
-    CLASSIFICATION_FILE = r"/home/tim/PycharmProjects/GridAnnotator/annotations_2013.json"
+    #CLASSIFICATION_FILE = r"/home/tim/PycharmProjects/GridAnnotator/annotations_2013.json"
+    CLASSIFICATION_FILE = r"/home/tdjg/PycharmProjects/GridAnnotator/annotations/training_set_annotations_2018.json"
     IMAGE_ZOOM_FACTOR = 2
